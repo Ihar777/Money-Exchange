d52ddcb78e21db3985cafd0240b429995c0ad2b8
Ihar <petrushen@yahoo.com>
2019-03-01 20:50:23 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,33 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let Hquan, Qquan, Dquan, Nquan, Pquan;
+   var exchange = {
+       H: 50,
+       Q: 25,
+       D: 10,
+       N: 5,
+       P: 1
+   };
+
+   Hquan = Math.floor(currency/exchange.H);
+   Qquan = Math.floor((currency - Hquan*exchange.H)/exchange.Q);
+   Dquan = Math.floor((currency - Hquan*exchange.H - Qquan*exchange.Q)/exchange.D);
+   Nquan = Math.floor((currency - Hquan*exchange.H - Qquan*exchange.Q - Dquan*exchange.D)/exchange.N);
+   Pquan = Math.floor((currency - Hquan*exchange.H - Qquan*exchange.Q - Dquan*exchange.D - Nquan*exchange.N)/exchange.P);
+
+   var result = {
+
+   }
+
+   if(Hquan>0){ result.H = Hquan };
+   if(Qquan>0){ result.Q = Qquan };
+   if(Dquan>0){ result.D = Dquan };
+   if(Nquan>0){ result.N = Nquan };
+   if(Pquan>0){ result.P = Pquan };
+
+   if(currency>10000){
+       return {error: "You are rich, my friend! We don't have so much coins for exchange"};
+   } else {
+    return result;
+   }
+
 }
